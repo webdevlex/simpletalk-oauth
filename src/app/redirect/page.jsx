558e630e154e4d2d00b2f1fbef6b83e70a060a21
Alexis Martin <alexismartin1895@gmail.com>
2024-07-23 10:58:13 -0700

    updated redirect url

--- src/app/redirect/page.jsx
+++ src/app/redirect/page.jsx
@@ -13,7 +13,7 @@ const Index = () => {
 			localStorage.setItem("token", token);
 			// Redirect to another website
 			window.location.href =
-				"https://marketplace.gohighlevel.com/oauth/chooselocation?response_type=code&redirect_uri=https://ghl.aicall.site&client_id=6646b83e3d03052997975239-lwa12jpm&scope=calendars.readonly calendars.write calendars/events.readonly calendars/events.write calendars/groups.readonly calendars/groups.write calendars/resources.readonly calendars/resources.write contacts.readonly contacts.write users.write users.readonly businesses.readonly businesses.write locations/customValues.readonly locations/customValues.write locations/customFields.readonly locations/customFields.write locations/tasks.readonly locations/tasks.write locations/tags.readonly locations/tags.write locations/templates.readonly"; // Replace with your desired website URL
+				"https://marketplace.gohighlevel.com/oauth/chooselocation?response_type=code&redirect_uri=https://ghl.aicall.site&client_id=6646b83e3d03052997975239-lwa12jpm&scope=calendars.readonly calendars.write calendars/events.readonly calendars/events.write calendars/groups.readonly calendars/groups.write calendars/resources.readonly calendars/resources.write contacts.readonly contacts.write users.write users.readonly businesses.readonly businesses.write locations/customValues.readonly locations/customValues.write locations/customFields.readonly locations/customFields.write locations/tasks.readonly locations/tasks.write locations/tags.readonly locations/tags.write locations/templates.readonly locations.readonly"; // Replace with your desired website URL
 		}
 	}, [searchParams]);
 
